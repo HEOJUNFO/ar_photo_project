f3b2ad94cfb3d4fc9d9cfb92df70c8a4220d0285
heojunfo <junhyeok.heo@culturecon.co.kr>
2023-09-26 20:15:47 +0900

    test

--- src/App.vue
+++ src/App.vue
@@ -109,11 +109,13 @@ export default {
         const checkBrowser = (useChange) => {
             let os = getOsName();
             let browser = getBrowserName();
-
+            alert(os + " " + browser)
             if (os.match(/iPhone|iPad|iPod/i)) { // 아이폰 접속 경우
                 // if(browser == 'kakao' || browser == 'whale')
                 if (browser == 'kakao') {
+                    console.log('kakao')
                     if (useChange == true) {
+                        console.log('add')
                         var url = window.location.href;
                         var link = url.slice(0, url.lastIndexOf("/")) + "/kakao.html";
                         location.href = link;
@@ -142,10 +144,9 @@ export default {
 
 
         onMounted(() => {
-            let os2 = getOsName();
-            let browser2 = getBrowserName();
 
-            alert(os2 + " " + browser2)
+
+
 
             checkBrowser(true)
 
